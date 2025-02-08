d6dd07395f03cf660ec98ff464488e4b64eab6eb
uxelf <uxself@gmail.com>
2025-02-08 04:36:20 +0100

    Script de bolas individuales

--- Assets/Assets/Player/Player2.cs
+++ Assets/Assets/Player/Player2.cs
@@ -1,59 +1,35 @@
 using UnityEngine;
+using static UnityEditor.Searcher.SearcherWindow.Alignment;
 
+
+[RequireComponent(typeof(BallMovementController))]
 public class Player2 : MonoBehaviour
 {
-    private float planetGravity = 9.81f * 2f;
-    public Vector3 gravityVector = Vector3.up * -1;
-    public float moveForce = 200;
-
-    private float gravityMult = 1;
-
-    private Rigidbody rb;
-    private Vector2 direction;
+    private BallMovementController controller;
 
     private void Awake()
     {
-        rb = GetComponent<Rigidbody>();
+        controller = GetComponent<BallMovementController>();
     }
 
-    private void Start()
-    {
-        Physics.gravity = planetGravity * gravityVector;
-    }
 
     private void Update()
     {
-        // Cambiar la gravedad con Right Shift (Shift derecho)
-        if (Input.GetKeyDown(KeyCode.RightShift))
+        if (Input.GetKeyDown(KeyCode.RightControl))
         {
-            gravityMult = 8;
-            UpdateGravity();
+            controller.ActivateExtraGravity();
         }
-        else if (Input.GetKeyUp(KeyCode.RightShift) && gravityMult == 8)
+        else if (Input.GetKeyUp(KeyCode.RightControl))
         {
-            gravityMult = 1;
-            UpdateGravity();
+            controller.DeactivateExtraGravity();
         }
 
-        // Movimiento usando el teclado numérico
-        float horizontal = 0f;
-        float vertical = 0f;
+        Vector2 direction = Vector2.zero;
+        if (Input.GetKey(KeyCode.Keypad4)) direction.x = -1; // Izquierda
+        else if (Input.GetKey(KeyCode.Keypad6)) direction.x = 1;  // Derecha
+        if (Input.GetKey(KeyCode.Keypad8)) direction.y = 1;    // Arriba (Adelante)
 
-        if (Input.GetKey(KeyCode.Keypad4)) horizontal = -1; // Izquierda
-        if (Input.GetKey(KeyCode.Keypad6)) horizontal = 1;  // Derecha
-        if (Input.GetKey(KeyCode.Keypad8)) vertical = 1;    // Arriba (Adelante)
-
-        direction = new Vector2(horizontal, vertical);
+        controller.SetInputDirection(direction);
     }
 
-    private void FixedUpdate()
-    {
-        // Aplica el movimiento basado en la dirección
-        rb.AddForce((direction.x * Vector3.right + direction.y * Vector3.forward) * moveForce);
-    }
-
-    private void UpdateGravity()
-    {
-        Physics.gravity = planetGravity * gravityVector * gravityMult;
-    }
 }
