bfeca19fdeab4f78bd8ae1c24c0247bf4e5fa401
uxelf <uxself@gmail.com>
2025-02-08 05:22:04 +0100

    Capped player velocity

--- Assets/Assets/Player/BallMovementController.cs
+++ Assets/Assets/Player/BallMovementController.cs
@@ -8,7 +8,8 @@ public class BallMovementController : MonoBehaviour
 
     private float movementForce = 20f;
     private Vector2 inputDirection = Vector2.zero;
-
+    private float maxForwardVelocity = 60;
+    private float maxHorizontalVelocity = 60;
     private void Awake()
     {
         rb = GetComponent<Rigidbody>();
@@ -38,6 +39,15 @@ public class BallMovementController : MonoBehaviour
         if (inputDirection != Vector2.zero) 
         {
             rb.AddForce(new Vector3(inputDirection.x, 0, inputDirection.y) * movementForce);
+        }
+
+        if (rb.linearVelocity.z > maxForwardVelocity)
+        {
+            rb.linearVelocity = new Vector3(rb.linearVelocity.x, rb.linearVelocity.y, maxForwardVelocity);
+        }
+        if (Mathf.Abs(rb.linearVelocity.x) > maxHorizontalVelocity)
+        {
+            rb.linearVelocity = new Vector3(Mathf.Sign(rb.linearVelocity.x), rb.linearVelocity.y, rb.linearVelocity.z);
         }
     }
 
